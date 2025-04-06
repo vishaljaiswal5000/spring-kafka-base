0a57b2a6d79bd7afaa7bd725c68c1e5ea59537d6
vishal Jaiswal <vishalJaiswal5000@gmail.com>
2025-04-06 14:49:56 +1000

    Initial Commit - Added producer, consumer and controller

---
+++ src/main/java/com/tv/spring_kafka/consumer/MessageConsumer.java
@@ -0,0 +1,14 @@
+package com.tv.spring_kafka.consumer;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageConsumer {
+
+    @KafkaListener(topics = "my-topic", groupId = "my-group-id")
+    public void listen(String message) {
+        System.out.println("Received message: " + message);
+    }
+
+}
